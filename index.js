0814af4d4450e14d3dfb4be133397a10399d3395
Anna K <anita.explorer@gmail.com>
2024-02-02 11:26:44 +0100

    Button refresh

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ function newSrc() {
   image2.src = "./images/dice" + number2 + ".png";
 
   if (number1 > number2) {
+    document.querySelector("#title").textContent = "Player 1 win!";
     console.log("Player 1 win!");
   } else if (number1 === number2) {
+    document.querySelector("#title").textContent = "Draw!";
     console.log("Draw!");
   } else {
+    document.querySelector("#title").textContent = "Player 2 win!";
     console.log("Player 2 win!");
   }
 }
